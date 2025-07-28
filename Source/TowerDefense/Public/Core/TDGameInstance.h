55f4d7f44282896874fdcf94c68a44867eb70d52
NikitaAksenov <naksenov96@gmail.com>
2025-07-28 20:05:17 +0300

    Added base classes

---
+++ Source/TowerDefense/Public/Core/TDGameInstance.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "TDGameInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOWERDEFENSE_API UTDGameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
